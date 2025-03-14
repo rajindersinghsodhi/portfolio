4fec069b0c1a2df58eb9258853754cda3dc962dc
rajindersinghsodhi <rajsodhisocial@gmail.com>
2025-03-14 16:40:33 +0530

    Feature(skills): shadcn carousel added

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+// tailwind.config.ts or tailwind.config.js
+import animate from "tailwindcss-animate";
+
+export default {
+  content: [
+    // your paths
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [animate],
+};
